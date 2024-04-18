31c87314c51b9aa7bb428f0876bc8232f97cadc8
Will Roberts <willeroberts3@gmail.com>
2024-04-17 19:29:49 -0500

    Pytests troubleshooting - add QApplication.

--- ellucitext/tests/test_main.py
+++ ellucitext/tests/test_main.py
@@ -1,17 +1,20 @@
 from gui.builder.code_editor_builder import CodeEditorBuilder
+from PySide6.QtWidgets import QApplication
 
-def test_main_window_displayed(qapp):
+app = QApplication([])
+
+def test_main_window_displayed():
     editor = CodeEditorBuilder().build("ElluciText", 800, 600)
     editor.show()
     assert editor.isVisible()
 
-def test_maximize_application(qapp):
+def test_maximize_application():
     editor = CodeEditorBuilder().build("ElluciText", 800, 600)
     editor.show()
     editor.showMaximized()
     assert editor.isMaximized()
 
-def test_minimize_application(qapp):
+def test_minimize_application():
     editor = CodeEditorBuilder().build("ElluciText", 800, 600)
     editor.show()
     editor.showMinimized()
